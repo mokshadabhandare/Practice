a7340a78a09501dceebf18fd5a38c5930fb3412f
mokshada <mokshadabhandare.01@gmail.com>
2024-02-29 11:57:55 +0530

    practice

--- MyConnection.session.sql
+++ MyConnection.session.sql
@@ -1,2 +1,9 @@
-CREATE TABLE class (id int primary key,name varchar(50),division varchar(40));
-INSERT into class(id,name,division)values(1,"MCA","Computer application");
+CREATE DATABASE IF NOT EXISTS college;
+
+CREATE table MCA1(id int primary key,name varchar(50),branch varchar(40),semester varchar(20));
+
+CREATE TABLE student(id int primary key,name varchar(40),age int,class varchar(40));
+
+SHOW table;
+
+drop DATABASE college;
