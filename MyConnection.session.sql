4332bbefa512ad1dc88d67e2998a80722f14b06e
mokshada <mokshadabhandare.01@gmail.com>
2024-03-03 11:31:21 +0530

    database

--- MyConnection.session.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE IF NOT EXISTS college;
-
-CREATE table MCA1(id int primary key,name varchar(50),branch varchar(40),semester varchar(20));
-
-CREATE TABLE student(id int primary key,name varchar(40),age int,class varchar(40));
-
-SHOW table;
-
-drop DATABASE college;
