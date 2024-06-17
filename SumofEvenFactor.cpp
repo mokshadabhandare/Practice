a534c903cb0db7a256fe365166c637448deb7465
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-06-17 23:19:12 +0530

    cmt

---
+++ SumofEvenFactor.cpp
@@ -0,0 +1,31 @@
+//write program to find factor of even and sum of factor
+
+#include<iostream>
+using namespace std;
+
+int SumOfFactors(int iNo)
+{
+    int iSum=0;
+     for(int i=1;i<iNo;i++)
+     {
+        if((iNo%i==0) && (i%2==0))
+        {
+            iSum=iSum+i;
+        }
+     }
+     return iSum;
+}
+int main()
+{
+    int iNo=0;
+
+    cout<<"Enter the number"<<endl;
+    cin>>iNo;
+    
+    int iRet=0;
+    iRet=SumOfFactors(iNo);
+    
+    cout<<"Sum of factors is"<<iRet <<endl;
+    
+    return 0;
+}
