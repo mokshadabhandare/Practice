e7d57cc3ca0f399551d7547dffee19bcea397435
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-06-17 21:32:58 +0530

    cmt

---
+++ Factorial.cpp
@@ -0,0 +1,18 @@
+//write a program to find factorial 
+
+#include<iostream>
+using namespace std;
+
+int findFactorial(int iNo)
+{
+    
+}
+int main()
+{
+    int iNo=0;
+
+    cout<<"Enter the number"<<endl;
+    cin>>iNo;
+
+    return 0;
+}
