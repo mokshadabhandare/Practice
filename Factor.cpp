75e2c3efb21d4fd6ce64c6c83b14b5de35d7bac9
mokshadabhandare <mokshadabhandare.01@gmail.com>
2024-06-17 22:27:47 +0530

    cmt

--- Factor.cpp
+++ Factor.cpp
@@ -1,8 +1,20 @@
+//find factors
+
 #include<iostream>
 using namespace std;
 
+void  findFactors(int iNo)
+{
+
+}
 int main()
 {
+    int iNo=0;
+
+    cout<<"Enter the number"<<endl;
+    cin>>iNo;
     
+    findFactors(iNo);
+
     return 0;
 }
